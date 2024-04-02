2f39fdca4b51fbdd0baa67b31ad5d4d666cae0d1
Victor Lima Rattes <victorrattes.dev@gmail.com>
2024-04-02 00:24:22 -0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeadbarComponent } from './sharepages/headbar/headbar.component';
-import { FooterComponent } from './sharepages/footer/footer.component';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeadbarComponent, FooterComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
